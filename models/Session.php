fb48fee56812166b555f1c37b71ab034da6c83ea
noa-cen <noacengarle@gmail.com>
2025-02-12 11:16:59 +0100

    session

---
+++ models/Session.php
@@ -0,0 +1,27 @@
+<?php
+
+class Session
+{
+    public function startSession()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public function checkUserLogin() 
+    {
+        if (!isset($_SESSION["user_id"])) {
+            header("Location: ../views/user/login.php");
+            exit();
+        }
+    }
+
+    public function logOut()
+    {
+        session_unset();
+        session_destroy();
+        header("Location: /projets/quiz_night/index.php");
+        exit();
+    }
+}
